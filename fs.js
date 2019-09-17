254aeb29366ae781cc24d303f17125dbc5aa7888
zyh <13105027620@163.com>
2019-09-17 15:08:34 +0800

    d

--- fs.js
+++ fs.js
@@ -1,15 +1,27 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-fs.open("server.js", "r+", (err, fd) => {
-    console.log(err, fd);
-})
+// fs.open("server.js", "r+", (err, fd) => {
+//     console.log(err, fd);
+// })
 
-fs.mkdir("./haha.js", (err) => {
-    if (err) {
-        console.log(err)
-    }
-});
+// fs.mkdir("./haha.js", (err) => {
+//     if (err) {
+//         console.log(err)
+//     }
+// });
 
-fs.stat("server.js",(err)=>{
-    console.log(err);
+// fs.stat("server.js",(err)=>{
+//     console.log(err);
+// })
+
+async function x() {
+    await y();
+}
+async function y() {
+    await x();
+}
+x().then((value)=>{
+    console.log(value)
+}).catch((e)=>{
+    console.log("&&&&&&&&&&",e)
 })
